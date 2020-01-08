e737cad08faa2118969971e7c3a65e2457188a9a
Sabbir Hossain <cse.sabbir@gmail.com>
2020-01-08 14:46:43 +0600

    latest code

--- js/text.js
+++ js/text.js
@@ -7,7 +7,7 @@ const data = [
     displayRatio: 0.9
   },
   {
-    text: `As a software engineer, My worst nightmare is that, one day someone told me, "So you are software engineer, can you fix my smartphone/computer?"\n My day dream, one day I arrived at office on time, write code all day without test, run code successfully on production environment with no bug and go back to home and take a sound sleep`,
+    text: `As a software engineer, My worst nightmare is that, one day someone told me, "So you are software engineer, can you fix my smartphone/computer?"\n My day dream, one day I arrived at office on time, write code all day without test, run code successfully on production environment with no bug and go back to home and take a sound sleep.\n And I also believe it will never happen.`,
     shadowText: ["N", "O", "D", "E", "_dot", "J", "S"],
     displayRatio: 0.9
   }
