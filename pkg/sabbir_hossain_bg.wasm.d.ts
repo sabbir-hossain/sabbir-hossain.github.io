9026a6281d18e87bebc9c4110a744444d88a6d11
sabbir-hossain <sabbir.hossain.dev@gmail.com>
2025-10-28 15:27:20 +0400

    Add WebAssembly binary and TypeScript definitions for Sabbir Hossain
    - Added a new WebAssembly binary file: pkg/sabbir_hossain_bg.wasm
    - Created TypeScript declaration file: pkg/sabbir_hossain_bg.wasm.d.ts
      - Exposed memory and various WebAssembly functions for integration

---
+++ pkg/sabbir_hossain_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run_web: () => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const closure3020_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2965_externref_shim: (a: number, b: number, c: any) => void;
+export const closure2961_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h7ca12409239dbcb0: (a: number, b: number) => void;
+export const __wbindgen_start: () => void;
